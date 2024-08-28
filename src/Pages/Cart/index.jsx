bf75a506c60d607e00647440c66a799be46c1f8e
unknown <z.hosseinpour86@gmail.com>
2024-08-28 14:06:36 +0330

    edit cart

--- src/Pages/Cart/index.jsx
+++ src/Pages/Cart/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { cartaction } from "../store/CartSlice";
+import { cartaction } from "../../Store/CartSlice";
 
 // component
 import Header from "../../Layout/Header";
