04379c7f0bea8f5ec57e56439d29854aa172c9d0
unknown <z.hosseinpour86@gmail.com>
2025-06-09 00:00:04 +0330

    edit category in mobile size

--- src/Pages/Category/index.jsx
+++ src/Pages/Category/index.jsx
@@ -29,7 +29,8 @@ const Category = () => {
   useEffect(() => {
     if (category1.length > 0) {
       setMenuItem(category1[0].title);
-      setActiveItem(category1[0].id), setMenuItemSlug(category1[0].slug);
+      setActiveItem(category1[0].id);
+      setMenuItemSlug(category1[0].slug);
     }
   }, []);
 
